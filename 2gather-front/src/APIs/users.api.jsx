e14e6ee928c7192b6d861f196f1ec208a01ec202
MoranSin <nurixhbh@gmail.com>
2024-03-28 18:59:24 +0200

    more modular but still does the error

--- 2gather-front/src/APIs/users.api.jsx
+++ 2gather-front/src/APIs/users.api.jsx
@@ -1,15 +1,13 @@
 import axios from "axios";
 
-const http = axios.create({
-    baseURL: "http://localhost:3000",
-});
+const baseURL = "http://localhost:3000/users/login/success";
 
-export const getUser = async () => {
-    try {
-        const url = "/users/login/success";
-        const { data } = await http.get(url);
-        return data;
-    } catch (err) {
-        console.log(err);
-    }
+export const authnicateUser = async () => {
+    return await axios.get(baseURL)
+        .then(response => {
+            return response.data;
+        })
+        .catch(err => console.log(err));
 }
+
+
--- 2gather-front/src/App.jsx
+++ 2gather-front/src/App.jsx
@@ -3,28 +3,22 @@ import { Routes , Route , Navigate } from 'react-router-dom'
 import Home from "./pages/Home/Home.jsx";
 import Login from "./pages/Login/Login.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
-import axios from "axios";
+// import axios from "axios";
 import './App.css'
 import Header from './components/Header/Header';
+import {authnicateUser} from './APIs/users.api.jsx';
 
 const App = () => {
     const [user,setUser] = useState(null);
 
     const getUser = async () => {
-        try{
-            const url = `http://localhost:3000/users/login/success`;
-           await axios.get(url)
-               .then(response => {
-                   setUser(response.data);
-               })
-               .catch(err => console.log(err));
-        } catch(err){
-            console.log(err)
-        }
+        const user = await authnicateUser();
+        console.log(user);
+        setUser(user);
     };
 
     useEffect(() => {
-        getUser();
+        getUser().then(() => console.log("User Authenticated"));
     }, []);
 
     return (
