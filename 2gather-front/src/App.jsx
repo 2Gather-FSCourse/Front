372fd472e7473ae36e7beb5e582610c334f25835
ronyzrihen <ronyzn3@gmail.com>
2024-03-28 20:42:02 +0200

    starting to work on campaigns list added api requests

--- 2gather-front/src/App.jsx
+++ 2gather-front/src/App.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect , useState } from 'react'
-import { Routes , Route , Navigate } from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {Routes, Route, Navigate, Link} from 'react-router-dom'
 import Home from "./pages/Home/Home.jsx";
 import Login from "./pages/Login/Login.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
+import Campaigns from "./pages/Campaigns/Campaigns.jsx";
 import axios from "axios";
 import './App.css'
 import Header from './components/Header/Header';
 
 const App = () => {
-    const [user,setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
     const getUser = async () => {
-        try{
+        try {
             const url = `http://localhost:3000/users/login/success`;
-           await axios.get(url)
-               .then(response => {
-                   setUser(response.data);
-               })
-               .catch(err => console.log(err));
-        } catch(err){
+            await axios.get(url)
+                .then(response => {
+                    setUser(response.data);
+                })
+                .catch(err => console.log(err));
+        } catch (err) {
             console.log(err)
         }
     };
@@ -29,11 +30,12 @@ const App = () => {
 
     return (
         <div className="App">
-            <Header />
+            <Header/>
             <Routes>
-            <Route exact path="/" element={user ? <Home user={user}/> : <Navigate to="/login" />} />
-            <Route exact path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-            <Route exact path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
+                    <Route exact path="/" element={<Campaigns/>}/>
+                    {/*<Route exact path="/" element={user ? <Home user={user}/> : <Navigate to="/login"/>}/>*/}
+                    {/*<Route exact path="/login" element={user ? <Navigate to="/"/> : <Login/>}/>*/}
+                    {/*<Route exact path="/signup" element={user ? <Navigate to="/"/> : <Signup/>}/>*/}
             </Routes>
         </div>
     );
