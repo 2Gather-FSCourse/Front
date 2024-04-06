352ae5273ec51c01ce360c8c9716b1cc8c460eb9
Adi Levi <adile0194@gmail.com>
2024-04-06 17:56:52 +0300

    finished donator wall

--- 2gather-front/src/pages/AppreciationWall/AppreciationWall.jsx
+++ 2gather-front/src/pages/AppreciationWall/AppreciationWall.jsx
@@ -8,21 +8,21 @@ const AppreciationWall = () => {
         <AppreciationWallContainer>
             <PageTitle title="Donor Appreciation Wall"/>
             <CirclesContainer>
-                <Circle color={'#B5D4E6'} size={45} opacity={0.2}></Circle>
-                <Circle color={'#B5D4E6'} size={53} opacity={0.65}></Circle>
-                <Circle color={'#91DFEE'} size={60} opacity={0.38}></Circle>
-                <Circle color={'#A594FF'} size={67} opacity={0.21}></Circle>
-                <Circle color={'#85A0EE'} size={75} opacity={0.21}></Circle>
-                <Circle color={'#91DFEE'} size={78} opacity={1}></Circle>
-                <Circle color={'#D0F5F7'} size={85} opacity={0.4}></Circle>
-                <Circle color={'#A594FF'} size={90} opacity={0.56}></Circle>
-                <Circle color={'#D0F5F7'} size={105} opacity={1}></Circle>
-                <Circle color={'#3DB2E2'} size={140} opacity={0.41}></Circle>
-                <Circle color={'#5093E7'} size={160} opacity={1}></Circle>
-                <Circle color={'#B7E2E4'} size={250} opacity={1}></Circle>
-                <Circle color={'#3DB2E2'} size={225} opacity={1}></Circle>
-                <Circle color={'#90A8F0'} size={260} opacity={1}></Circle>
-                <Circle color={'#242D47'} size={275} opacity={1}></Circle>
+                <Circle color={'#B5D4E6'} height={4.4} width={3.1} opacity={0.2} bottom={2}></Circle>
+                <Circle color={'#B5D4E6'} height={5.1} width={3.6} opacity={0.65} top={4.8} right={3.4}></Circle>
+                <Circle color={'#91DFEE'} height={5.8} width={4.1} opacity={0.38} right={5.5}></Circle>
+                <Circle color={'#A594FF'} height={7.3} width={5.2} opacity={0.21} top={2.9} right={5.5}></Circle>
+                <Circle color={'#85A0EE'} height={9.76} width={6.9} opacity={0.21} top={9.75} right={18.4}></Circle>
+                <Circle color={'#91DFEE'} height={7.8} width={5.5} opacity={1} top={21.4} right={22.9}></Circle>
+                <Circle color={'#D0F5F7'} height={6.8} width={4.8} opacity={0.4} top={3.9} right={22.2}></Circle>
+                <Circle color={'#A594FF'} height={11.7} width={8.3} opacity={0.56} top={13} right={31.25}></Circle>
+                <Circle color={'#D0F5F7'} height={11.7} width={8.3} opacity={1} top={6.8} right={31.9}></Circle>
+                <Circle color={'#3DB2E2'} height={14.1} width={10} opacity={0.41} top={30} right={55}></Circle>
+                <Circle color={'#5093E7'} height={14.6} width={10.4} opacity={1} top={10} right={43}></Circle>
+                <Circle color={'#B7E2E4'} height={24.4} width={17.3} opacity={1} bottom={17} left={20}></Circle>
+                <Circle color={'#3DB2E2'} height={21.9} width={15.6} opacity={1} top={7} right={5}></Circle>
+                <Circle color={'#90A8F0'} height={25.3} width={18} opacity={1} bottom={25} left={5}></Circle>
+                <Circle color={'#242D47'} height={34.1} width={24.3} opacity={1} top={3} right={23}></Circle>
             </CirclesContainer>
         </AppreciationWallContainer>
     );
--- 2gather-front/src/pages/AppreciationWall/AppreciationWall.style.js
+++ 2gather-front/src/pages/AppreciationWall/AppreciationWall.style.js
@@ -4,9 +4,14 @@ export const AppreciationWallContainer = styled.div`
     display: flex;
     flex-direction: column;
     margin: 1% 15%;
+height: 85vh;
 `;
 
 export const CirclesContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
-  `;
+    justify-content: center;
+    padding-left: 10%;
+    width: 100%;
+    height: 100%;
+`;
