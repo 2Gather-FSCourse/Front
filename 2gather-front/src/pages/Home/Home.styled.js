0097d24f9ec03c799ba80a6a4324533330b55e3d
ronyzrihen <ronyzn3@gmail.com>
2024-04-08 15:10:46 +0300

    added routing from main page to other pages

--- 2gather-front/src/pages/Home/Home.styled.js
+++ 2gather-front/src/pages/Home/Home.styled.js
@@ -8,7 +8,7 @@ export const HomeWrapperStyled = styled.div`
     gap: 50px;
     width: 100%;
     height: 100%;
-    //padding: 1rem;
+    padding: 50px 0;
     `;
 
 export const HomeCardRowStyled = styled.div`
